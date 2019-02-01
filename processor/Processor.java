247a9942c63dbcd7923adf6d5c566426cda24147
Jatish Khanna <jatish.khanna@gmail.com>
2019-02-01 06:57:50 +0100

    Update Processor.java

--- processor/Processor.java
+++ processor/Processor.java
@@ -1,12 +1,6 @@
 package processor;
 
 /**
-*/
-
-public interface Processor {
-package processor;
-
-/**
  * An interface defines the processor - A processor is a unit that process all
  * the messages for a exchange - Different implementation of processors are
  * possible (synch, async, or random)
@@ -19,8 +13,4 @@ package processor;
 public interface Processor {
 
 	void process(Exchange exchange);
-
-}
-  void process(Exchange exchange);
-  
 }
